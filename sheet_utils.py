9ffab360f14ee2dd9af54255409894b92cab18c9
Soujanya-R <soujanyaravi26@gmail.com>
2026-02-17 13:09:24 +0530

    Ignore credentials.json and update sheet_utils for env variable

--- sheet_utils.py
+++ sheet_utils.py
@@ -1,6 +1,8 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import pandas as pd
+import json
+import os
 
 def connect_sheet(sheet_name):
     scope = [
@@ -8,16 +10,18 @@ def connect_sheet(sheet_name):
         "https://www.googleapis.com/auth/drive"
     ]
 
-    creds = ServiceAccountCredentials.from_json_keyfile_name(
-        "credentials.json", scope
-    )
+    # Load credentials from environment variable
+    creds_json = os.environ.get("GOOGLE_CREDS_JSON")
+    if not creds_json:
+        raise ValueError("Set the GOOGLE_CREDS_JSON environment variable!")
 
+    creds_dict = json.loads(creds_json)
+
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
     client = gspread.authorize(creds)
-    sheet = client.open_by_url("https://docs.google.com/spreadsheets/d/1k3oq9NAW-X-gnMYclA68R7qPzB7XRSCU4CV9VeeZCxM/edit?gid=1597532669#gid=1597532669")
 
-
+    sheet = client.open(sheet_name)
     return sheet
-
 
 def load_data(sheet, worksheet_name):
     ws = sheet.worksheet(worksheet_name)
@@ -28,4 +32,3 @@ def load_data(sheet, worksheet_name):
     df.columns = df.columns.str.strip()
 
     return df, ws
-
